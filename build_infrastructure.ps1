a3c6fa480a0c8785a7b4a39d86c7216eb62df9e8
Benjamin Mitchell <Benjamin Mitchell>
2017-03-03 21:59:20 -0500

    updated infrastructure json

--- build_infrastructure.ps1
+++ build_infrastructure.ps1
@@ -1,9 +1,9 @@
-Login-AzureRmAccount -Credential (Get-Credential) -SubscriptionName 'LAB'
-$resourceGroup = 'lab-test-01'
-$TempUri = "https://bitbucket.org/BenMitchell1979/azure-lab-test/raw/619f97963d0a685ef8aba62975e518c79bd8b5d8/infrastructure.json"
+#Login-AzureRmAccount -Credential (Get-Credential)
+$resourceGroup = 'cgnlabs-azure'
+$TempUri = "https://raw.githubusercontent.com/BenMitchell1979/cgnlab-azure/master/infrastructure.json"
 
 #Validate if RG exists and if not build it
-New-AzureRmResourceGroup -Name "$resourceGroup" -Location 'eastus' -force
+New-AzureRmResourceGroup -Name "$resourceGroup" -Location 'eastus2' -force
 
-New-AzureRmResourceGroupDeployment -Name 'lab-test-infrastructure' -ResourceGroupName "$resourceGroup" -TemplateUri $TempUri
+New-AzureRmResourceGroupDeployment -Name 'cgnlabs-infrastructure' -ResourceGroupName "$resourceGroup" -TemplateUri $TempUri
 
