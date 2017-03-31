2870b02fef7f99fb35a951fe808cf2bcb01107d5
Benjamin M. Mitchell <bmitchell@complete-geek.com>
2017-03-31 15:46:52 -0400

    updated for AzureAutomation Run

--- build_infrastructure.ps1
+++
@@ -1,9 +0,0 @@
-#Login-AzureRmAccount -Credential (Get-Credential)
-$resourceGroup = 'cgnlabs-azure'
-$TempUri = "https://raw.githubusercontent.com/BenMitchell1979/cgnlab-azure/master/infrastructure.json"
-
-#Validate if RG exists and if not build it
-New-AzureRmResourceGroup -Name "$resourceGroup" -Location 'eastus2' -force
-
-New-AzureRmResourceGroupDeployment -Name 'cgnlabs_v1' -ResourceGroupName "$resourceGroup" -TemplateUri $TempUri
-
